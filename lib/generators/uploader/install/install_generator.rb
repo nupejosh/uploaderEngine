5af1fbe57cd7855ba755c0f05b4890c2441033db
wolfpack <wolfpack@vulk.co>
2015-08-15 18:16:18 -0400

    adding migration and nodel methods to install generator

--- lib/generators/uploader/install/install_generator.rb
+++ lib/generators/uploader/install/install_generator.rb
@@ -1,3 +1,17 @@
 class Uploader::InstallGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+
+  inject_into_file 'upload.rb', after: "class Upload < ActiveRecord::Base \n" do <<-'RUBY'
+  has_many #{plural_name}
+  RUBY
+  end   
+
+  migration_template "migration.rb", "db/migrate/add_#{name}_id_to_bar.rb"
+
+  def migration_data
+    <<RUBY
+      ## Resource Relationship
+      t.integer :#{name}_id,  
+RUBY
+  end      
 end
